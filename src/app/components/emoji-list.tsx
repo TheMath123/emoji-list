240e54663f15976b157de0c76f0f9ec816f4b544
Matheus P Agostinho <contact@matheuspa.com>
2024-12-24 16:58:39 -0300

    Add emoji search functionality and favicon to layout

--- src/app/components/emoji-list.tsx
+++ src/app/components/emoji-list.tsx
@@ -1,24 +1,45 @@
+'use client';
+import { useState } from 'react';
 import { Emogi } from "@/@types/emoji";
 import { EmojiDisplay } from "./emoji";
+import { Search } from "lucide-react";
 
-export async function EmojiList() {
-  const response = await fetch(`${process.env.URL_API}?access_key=${process.env.API_KEY}`);
-  if (!response.ok) {
-    return <div>Failed to fetch data</div>;
-  }
-  const data = await response.json();
+interface EmojiListProps {
+  data: Emogi[];
+}
 
-  // Verifica se data é um array
+export function EmojiList({ data }: EmojiListProps) {
+  const [searchTerm, setSearchTerm] = useState('');
+
   const emojis: Emogi[] = Array.isArray(data) ? data : [];
 
-  // Adiciona log para debug
-  console.log('Dados recebidos:', data);
+  const filteredEmojis = emojis.filter((emoji) => {
+    const searchLower = searchTerm.toLowerCase();
+    return (
+      emoji.slug.toLowerCase().includes(searchLower) ||
+      emoji.group.toLowerCase().includes(searchLower) ||
+      emoji.unicodeName.toLowerCase().includes(searchLower)
+    );
+  });
 
   return (
-    <main className="grid grid-cols-4 md:grid-cols-6 xl:grid-cols-6 2xl:grid-cols-10">
-      {emojis.map((emoji) => (
-        <EmojiDisplay key={emoji.slug} {...emoji} />
-      ))}
-    </main>
+    <div className="space-y-4 relative pt-8">
+      <div className="fixed inset-y-0 left-0 z-10 flex flex-row gap-2 items-center bg-white border-gray-300 border px-4 h-12 w-full">
+        <Search className="h-5 w-5 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search by name, group or unicode name..."
+          className="w-full focus:ring-2 py-2 focus:ring-blue-500 focus:border-blue-500"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
+      <main className="grid grid-cols-4 md:grid-cols-6 xl:grid-cols-6 2xl:grid-cols-10">
+        {filteredEmojis.map((emoji) => (
+          <EmojiDisplay key={emoji.slug} {...emoji} />
+        ))}
+      </main>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { EmojiList } from "./components/emoji-list";
 
 export default async function Home() {
-  return <EmojiList />
+  const response = await fetch(`${process.env.URL_API}?access_key=${process.env.API_KEY}`);
+  if (!response.ok) {
+    return <div>Failed to fetch data</div>;
+  }
+  const data = await response.json();
+
+  return <EmojiList data={data} />
 }
